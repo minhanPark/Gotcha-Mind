78ba70c257a84dbefbaef6d11f91fb3fc015e222
unknown <alsgks1218@naver.com>
2019-08-16 09:45:17 +0900

    io(/) is connect server command

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { join } from "path";
 import socketIO from "socket.io";
+import logger from "morgan";
 
 const PORT = 4000;
 const app = express();
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
 app.use(express.static(join(__dirname, "static")));
+app.use(logger("dev"));
 
 app.get("/", (req, res) => res.render("home"));
 
@@ -15,4 +17,12 @@ const handleListening = () =>
 
 const server = app.listen(PORT, handleListening);
 
-const io = socketIO(server);
+const io = socketIO.listen(server);
+
+const sockets = [];
+
+io.on("connection", socket => {
+  sockets.push(socket.id);
+});
+
+setInterval(() => console.log(sockets), 1000);
--- src/static/index.js
+++ src/static/index.js
@@ -1,1 +1,1 @@
-console.log("ME ToO");
+io("/");
