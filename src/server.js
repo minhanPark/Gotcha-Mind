06fb8b24c1115024c24225d526a99df5e5736deb
unknown <alsgks1218@naver.com>
2019-08-20 11:40:47 +0900

    add global variables and code refactoring

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import { join } from "path";
 import socketIO from "socket.io";
 import logger from "morgan";
 import socketController from "./socketController";
+import events from "./events";
 
 const PORT = 4000;
 const app = express();
@@ -11,7 +12,9 @@ app.set("views", join(__dirname, "views"));
 app.use(express.static(join(__dirname, "static")));
 app.use(logger("dev"));
 
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (req, res) =>
+  res.render("home", { events: JSON.stringify(events) })
+);
 
 const handleListening = () =>
   console.log(`Server running: http://localhost:${PORT}`);
